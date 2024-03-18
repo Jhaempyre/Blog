386213818507d0c3a115861044e16009ba34d092
jhaemyre <jhaashish.ajha@gmail.com>
2024-03-18 09:56:14 +0530

    added tailwind and some auth code

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,10 +1,29 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import {useDispatch} from 'react-redux'
 import './App.css'
+import authService from './appwrite/auth'
+import { login, logout } from './store/authSlice'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [loading ,  setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+
+  useEffect(()=>{
+    authService.getCurrentUser
+    .then((userData)=>{
+      if (userData){
+        dispatch(login(userData))
+      }else{
+        dispatch(logout())
+      }
+
+    })
+    .finally( () => {setLoading(false)})
+  },[])
 
   return (
     <>
