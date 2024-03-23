0942b35fa1872a6058e60c999a91fd4b588a539d
jhaemyre <jhaashish.ajha@gmail.com>
2024-03-24 00:47:05 +0530

    comment added and necessary chekup app is still crashing

--- blog/src/store/store.js
+++ blog/src/store/store.js
@@ -2,10 +2,11 @@ import {configureStore} from "@reduxjs/toolkit"
 import authSlice from "./authSlice"
 //import { combineReducers } from "@reduxjs/toolkit"
 
+// getting reducers which we have made so that we can acces the content without using context api 
 const store = configureStore({
     reducer : {
         auth : authSlice,
     }
-})
+});
 
-export default store
+export default store ;
