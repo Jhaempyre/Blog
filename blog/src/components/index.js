12e212bfd33daeafd10ff428cea68cf03ed23b25
jhaemyre <jhaashish.ajha@gmail.com>
2024-03-23 19:31:07 +0530

    added files for post realtime editor and exporting files

--- blog/src/components/index.js
+++ blog/src/components/index.js
@@ -3,10 +3,17 @@ import Footer from "./Footer/Footer.jsx"
 import container from "./container/container.js"
 import Logo from "./Logo.jsx"
 import LogoutBtn from "./Header/logoutBtn.jsx"
+import SignIn from "./SignIn.jsx"
+import AuthLayout from "./AuthLayout.jsx" 
+import PostCard from "./PostCard.jsx"
+
 export {
     Footer , 
     Header ,
     container ,
     Logo ,
-    LogoutBtn
+    LogoutBtn,
+    SignIn,
+    AuthLayout,
+    PostCard
 }
