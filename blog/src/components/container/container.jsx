320aaf70c8c2240351e4bcf80ac8882024249c6a
jhaemyre <jhaashish.ajha@gmail.com>
2024-03-24 00:58:56 +0530

    containe comment added , app still crashes

--- blog/src/components/container/container.jsx
+++ blog/src/components/container/container.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 
-function Container({children}){
+// This component acts as a wrapper to provide styling and structure to its children components.
+// It utilizes the `children` prop, a special prop in React that represents any components or elements nested inside this component.
+// By wrapping its children inside a div with specific styling classes, it ensures consistent layout and appearance across different parts of the application.
+// This component can be used to encapsulate sections of the UI and maintain a clean and organized structure.
+function Container({ children }) {
     return (
-    <div className='w-full max-w-7xl mx-auto px-4'>{children}</div>
-    )
+        <div className='w-full max-w-7xl mx-auto px-4'>
+            {children}
+        </div>
+    );
 }
 
-export default Container
+export default Container;
